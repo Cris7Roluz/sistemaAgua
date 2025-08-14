f4c851d0921ee13898e73dcb7f36d44713f13a48
Cris7Roluz <lizchuquin@gmail.com>
2025-08-14 17:41:56 -0500

    Add files via upload

---
+++ galeria.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet"crossorigin="anonymous">
+        <link rel="stylesheet" href="css/lightbox.min.css">
+        <title>Rinconada</title>
+    </head>
+    <style>
+        .lightbox-gallery{background-image: linear-gradient(#4A148C, #E53935);background-repeat: no-repeat;color: #000;overflow-x: hidden}.lightbox-gallery p{color:#fff}.lightbox-gallery h2{font-weight:bold;margin-bottom:40px;padding-top:40px;color:#fff}@media (max-width:767px){.lightbox-gallery h2{margin-bottom:25px;padding-top:25px;font-size:24px}}.lightbox-gallery .intro{font-size:16px;max-width:500px;margin:0 auto 40px}.lightbox-gallery .intro p{margin-bottom:0}.lightbox-gallery .photos{padding-bottom:20px}.lightbox-gallery .item{padding-bottom:30px}
+    </style>
+    <body>
+    <div class="">
+        <!-- NAVAR-->
+        <nav class="navbar navbar-expand-md navbar-dark sticky-top" style="background-color: #407a51;">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="img/logo.svg" alt="" width="30" height="30" class="d-inline-block align-text-top">
+                <small>Comunidad <b>&#8220;La Rinconada&#8221;</b></small>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto  mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="index.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" href="galeria.php">Galer&iacute;a</a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Sistema
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="system/logear.php">Ingresar</a></li>
+                    <li><a class="dropdown-item" href="#">Consultar Factura</a></li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="contacto.php">Contactos</a>
+                </li>
+            </ul>            
+            </div>
+        </div>
+        </nav>
+        <!-- FIN NAVAR-->
+        <!-- GAleria -->
+        <div class="container my-5">
+            <div class="intro">
+                <h2 class="text-center">Galer&iacute;a</h2>
+                
+            </div>
+            <div class=" py-5 row">
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/1.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/1.jpg" ></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/2.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/2.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/3.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/3.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/4.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/4.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/5.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/5.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/6.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/6.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/7.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/7.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/8.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/8.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/9.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/9.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/10.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/10.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/11.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/11.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/12.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/12.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/13.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/13.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/14.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/14.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/15.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/15.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/16.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/16.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/17.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/17.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/18.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/18.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/19.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/19.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/20.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/20.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/21.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/21.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/22.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/22.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/23.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/23.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/24.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/24.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/25.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/25.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-3 item"><a href="img/galeria/26.jpg" data-lightbox="photos"><img class="img-fluid" src="img/galeria/26.jpg"></a></div>
+            </div>
+        </div>
+    </div>
+
+<!-- GAleria -->
+    </div>
+
+    <!-- Footer-->
+    <footer class="bg-dark text-white mt-4 p-3  fixed-bottom">
+        <div class="text-center " style="background-color: rgba(0, 0, 0, 0.025);">
+            © 2023 Copyright:
+            <a class="text-reset fw-bold" href="#">Rinconada.com</a>
+        </div>
+    </footer>
+     <!-- FIN DE Footer-->
+        <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/lightbox.min.js"></script>
+    </body>
+</html>
---
+++ index2.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/lightbox.min.css"> 
+        <title>La Rinconada</title>
+    </head>
+    <body>
+        <!-- NAVAR-->
+        <nav class="navbar navbar-expand-md navbar-dark sticky-top" style="background-color: #407a51;">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="img/logo.svg" alt="" width="30" height="30" class="d-inline-block align-text-top">
+                <small>Comunidad <b>&#8220;La Rinconada&#8221;</b></small>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto  mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="galeria.php">Galer&iacute;a</a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Sistema
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="system/logear.php">Ingresar</a></li>
+                    <li><a class="dropdown-item" href="#">Consultar Factura</a></li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="contacto.php">Contactos</a>
+                </li>
+            </ul>                
+            </div>
+        </div>
+        </nav>
+        <!-- FIN NAVAR-->
+        <!-- SLIDER-->
+        <div class="container py-2 ">
+        <div class="card shadow rounded">
+        <div id="carouselExampleIndicators" class="carousel slide  " data-bs-ride="carousel" >
+        <div class="carousel-indicators" >
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner rounded" >
+            <div class="carousel-item active">
+            <img src="img/1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="img/2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="img/3.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="img/4.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="img/5.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="img/6.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="img/7.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Anterior</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Siguiente</span>
+        </button>
+    </div>
+    </div>
+    </div>
+    <!--FIN SLIDER-->
+    <!-- TARJETAS o CARDS-->
+    <!-- SECCION UNO-->
+    <section class="container">
+        <figure class="text-center">
+            <blockquote class="blockquote">
+                <h2><strong>Bienvenidos a La Rinconada</strong></h2>
+            </blockquote>
+            <figcaption class="blockquote-footer">
+                El Valle de los Arcoiris  <cite title="Source Title"></cite>
+            </figcaption>
+        </figure>
+    <!--CARDS -->
+    <div class="card " style="max-width: 100%;">
+        <div class="row g-0">
+            
+            <div class="col-md-12 ">
+            <div class="card shadow">
+                <div class="card-body">
+                    <p class="card-text">En la provincia de <strong>Imbabura</strong> canton <strong>Ibarra</strong> a sur oriente podemos encontrar la localidad <strong> La Rinconada aproximadamente a unos 45 minutos</strong>, donde se puede disfrutar y experimentar varias actividades de aventura sobre todo la escalada llena de adrenalina, será una experiencia inolvidable.
+                    <b></b>
+                    
+                    Visita nuestros atractivos turisticos de la comunidad, cual es conocida por  tener fuentes de agua y paramos con diversidad  de flora y fauna, es una comunidad  netamente dedica  al cuidado de paramos y a sus fuentes de agua, ya que gracias a ellos pueden disfrutar de salud y vida. </p>
+                        <ol>
+                            <li>P&aacute;ramo.</li>
+                            <li>Casacadas.</li>
+                            <li>Fuentes de aguas.</li>
+                        </ol>
+                    <p>El turista podrá disfrutar y vivir el verdadero contacto con la naturaleza, la visita de los p&aacute;ramos podr&aacute; admirar y conocer las distintas especies de animales que habitan el mismo, los mismo que mencionamos a continuaci&oacute;n:</p>
+                    <ol>
+                        <li>Oso Andino</li>
+                        <li>Cóndor</li>
+                        <li>Venados</li>
+                        <li>Lobos</li>
+                        <li>Zorrillos</li>
+                        <li>Conejos</li>
+                        <li>y muchas m&aacute;s especies</li>
+                    </ol>            
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+    
+    <!-- FIN CARDS -->
+    <!-- FIN DE TARJETAS o CARDS-->
+    </div>
+    <div class="container pb-5 py-4 ">
+    <!-- Footer-->
+    <footer class="bg-dark  text-white mt-4 p-3 fixed-bottom">
+        <!-- Grid container -->
+        <div class="text-center " style="background-color: rgba(0, 0, 0, 0.025);">
+            &copy; 2023 Copyright:
+            <a class="text-reset fw-bold" href="#">La Rinconada</a>
+        </div>
+    </footer>
+    <!-- FIN DE Footer-->
+        <script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    </body>
+</html>
+
